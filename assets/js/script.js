0345a41298fdc5ec8ae7ade1ce44f4e7a6322ab8
MichealC03 <micheal.callahan@icloud.com>
2023-12-19 11:19:04 -0700

    cleaning out projects that arent mine

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,19 +59,15 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Micheal Callahan";
             $("#favicon").attr("href", "assets/images/favicon.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
         }
     });
 
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["data analytics", "backend development", "machine learning", "software development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
