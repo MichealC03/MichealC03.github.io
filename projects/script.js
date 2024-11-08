94107ff63944ef4d25638257f28cba5e850b7e6f
MichealC03 <micheal.callahan@icloud.com>
2024-11-08 01:19:16 -0700

    claude js method

--- projects/script.js
+++ projects/script.js
@@ -36,90 +36,65 @@ function getProjects() {
 }
 
 
+// JavaScript implementation
 function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt">
-          <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-          <div class="content">
-            <div class="tag">
-              <h3>${project.name}</h3>
-            </div>
-            <div class="desc">
-              <p>${project.desc}</p>
-              <div class="btns">
-                ${project.links.view ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i></a>` : ''}
-                ${project.links.demo ? `<a href="${project.links.demo}" class="btn" target="_blank"><i class="fas fa-video"></i></a>` : ''}
-                ${project.links.code ? `<a href="${project.links.code}" class="btn" target="_blank"><i class="fas fa-code"></i></a>` : ''}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>`
+    const projectsContainer = document.querySelector(".work .box-container");
+    const filterButtons = document.querySelectorAll("#filters button");
+    
+    // Function to render projects
+    function renderProjects(filterCategory = '*') {
+        const projectsHTML = projects
+            .filter(project => {
+                // Show all projects if filterCategory is '*', otherwise filter by category
+                return filterCategory === '*' || project.category === filterCategory;
+            })
+            .map(project => `
+                <div class="grid-item ${project.category}">
+                    <div class="box tilt">
+                        <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+                        <div class="content">
+                            <div class="tag">
+                                <h3>${project.name}</h3>
+                            </div>
+                            <div class="desc">
+                                <p>${project.desc}</p>
+                                <div class="btns">
+                                    ${project.links.view ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i></a>` : ''}
+                                    ${project.links.demo ? `<a href="${project.links.demo}" class="btn" target="_blank"><i class="fas fa-video"></i></a>` : ''}
+                                    ${project.links.code ? `<a href="${project.links.code}" class="btn" target="_blank"><i class="fas fa-code"></i></a>` : ''}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `)
+            .join('');
+            
+        projectsContainer.innerHTML = projectsHTML;
+    }
+
+    // Add click event listeners to filter buttons
+    filterButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Remove is-checked class from all buttons
+            filterButtons.forEach(btn => btn.classList.remove('is-checked'));
+            
+            // Add is-checked class to clicked button
+            this.classList.add('is-checked');
+            
+            // Get filter value and render filtered projects
+            const filterValue = this.getAttribute('data-filter');
+            renderProjects(filterValue);
+        });
     });
-    projectsContainer.innerHTML = projectsHTML;
 
-    $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-
-    // vanilla tilt.js
-    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    //     max: 20,
-    // });
-    // // vanilla tilt.js  
-
-    // /* ===== SCROLL REVEAL ANIMATION ===== */
-    // const srtop = ScrollReveal({
-    //     origin: 'bottom',
-    //     distance: '80px',
-    //     duration: 1000,
-    //     reset: true
-    // });
-
-    // /* SCROLL PROJECTS */
-    // srtop.reveal('.work .box', { interval: 200 });
-
-    // // isotope filter products
-    // var $grid = $('.box-container').isotope({
-    //     itemSelector: '.grid-item',
-    //     layoutMode: 'fitRows',
-    //     masonry: {
-    //         columnWidth: 200
-    //     }
-    // });
-
-    // // filter items on button click
-    // $('.button-group').on('click', 'button', function () {
-    //     $('.button-group').find('.is-checked').removeClass('is-checked');
-    //     $(this).addClass('is-checked');
-    //     var filterValue = $(this).attr('data-filter');
-    //     $grid.isotope({ filter: filterValue });
-    // });
+    // Initial render of all projects
+    renderProjects('*');
 }
 
 getProjects().then(data => {
     showProjects(data);
 })
-// fetch projects end
-
-// Start of Tawk.to Live Chat
-// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-// (function () {
-//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-//     s1.async = true;
-//     s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-//     s1.charset = 'UTF-8';
-//     s1.setAttribute('crossorigin', '*');
-//     s0.parentNode.insertBefore(s1, s0);
-// })();
-// // End of Tawk.to Live Chat
 
 // disable developer mode
 document.onkeydown = function (e) {
