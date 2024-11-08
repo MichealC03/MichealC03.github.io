dc73b0785ea74b2993c67783e0467e4198e9a3f9
MichealC03 <micheal.callahan@icloud.com>
2024-11-08 00:59:10 -0700

    optional values

--- projects/script.js
+++ projects/script.js
@@ -51,9 +51,9 @@ function showProjects(projects) {
             <div class="desc">
               <p>${project.desc}</p>
               <div class="btns">
-                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i>View Post</a>
-                <a href="${project.links.demo}" class="btn" target="_blank"><i class="fas fa-video"></i>Video</a>
-                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                ${project.links.view ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>` : ''}
+                ${project.links.demo ? `<a href="${project.links.demo}" class="btn" target="_blank"><i class="fas fa-video"></i> Demo</a>` : ''}
+                ${project.links.code ? `<a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>` : ''}
               </div>
             </div>
           </div>
